a9167fa79058f46b9cd94a5fc21943e578091d94
TelmoCode <telmogferreira@gmail.com>
2022-10-03 07:48:51 -0300

    iniciar variaveis e validação dos campos de input

--- app/src/main/java/com/telmocode/beberagua_lembrete/MainActivity.kt
+++ app/src/main/java/com/telmocode/beberagua_lembrete/MainActivity.kt
@@ -2,12 +2,47 @@ package com.telmocode.beberagua_lembrete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editPeso: EditText
+    private lateinit var editIdade: EditText
+    private lateinit var btn_calc: Button
+    private lateinit var txtResultadoMl: TextView
+    private lateinit var btn_refresh: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+        iniciarComponentes()
+
+        btn_calc.setOnClickListener {
+            if(editPeso.text.toString().isEmpty()){
+                Toast.makeText(this,R.string.touch_Peso, Toast.LENGTH_SHORT).show()
+
+
+            }else if(editIdade.text.toString().isEmpty()) {
+                Toast.makeText(this, R.string.touch_idade, Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
     }
+    private fun iniciarComponentes(){
+        editPeso = findViewById(R.id.ipt_peso)
+        editIdade = findViewById(R.id.ipt_idade)
+        btn_calc = findViewById(R.id.btn_calc)
+        txtResultadoMl = findViewById(R.id.resultado_ml)
+        btn_refresh = findViewById(R.id.refresh)
+    }
+
 }
